2f7df6ba8994792c472c6a46a6d75d63a59a3a21
Thorben Denzer <denzer@atix.de>
2026-01-07 09:37:59 +0100

    build: bump version to 0.3.3

--- lib/foreman_ansible_director/constants.rb
+++ lib/foreman_ansible_director/constants.rb
@@ -3,7 +3,7 @@
 module ForemanAnsibleDirector
   module Constants
     PLUGIN_NAME = 'foreman_ansible_director'
-    PLUGIN_VERSION = '0.3.2'
+    PLUGIN_VERSION = '0.3.3'
 
     DEFAULT_GALAXY_URL = 'https://galaxy.ansible.com/'
     DEFAULT_ANSIBLE_VERSION = '2.19.3'
