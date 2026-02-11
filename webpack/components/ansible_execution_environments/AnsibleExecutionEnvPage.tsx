eea6f82a9749940738ae69cc849e837bff62a211
Thorben Denzer <denzer@atix.de>
2026-02-11 15:02:51 +0100

    feat(ui/execution_environments): add search-bar to execution environment index page

--- webpack/components/ansible_execution_environments/AnsibleExecutionEnvPage.tsx
+++ webpack/components/ansible_execution_environments/AnsibleExecutionEnvPage.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 
 import { translate as _ } from 'foremanReact/common/I18n';
+import { useUrlParams } from 'foremanReact/components/PF4/TableIndexPage/Table/TableHooks';
 
 import ExecutionEnvGridWrapper from './components/ExecutionEnvGridWrapper';
 import { Page } from '../common/Page';
 
-const AnsibleExecutionEnvPage: React.FC = () => (
-  <Page
-    header={_('Ansible Execution Environments')}
-    hasDocumentation={false}
-    customToolbarItems={[]}
-  >
-    <ExecutionEnvGridWrapper />
-  </Page>
-);
+const AnsibleExecutionEnvPage: React.FC = () => {
+  const { searchParam } = useUrlParams();
+  return (
+    <Page
+      header={_('Ansible Execution Environments')}
+      hasDocumentation={false}
+      customToolbarItems={[]}
+    >
+      <ExecutionEnvGridWrapper initialSearch={searchParam} />
+    </Page>
+  );
+};
 
 export default AnsibleExecutionEnvPage;
