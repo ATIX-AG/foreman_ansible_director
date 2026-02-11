9fd72f92a0ff5c592970761627552fc5a3748a13
Thorben Denzer <denzer@atix.de>
2026-02-11 15:30:12 +0100

    refactor(lce/paths)!: remove unused swap_environments method

--- app/models/foreman_ansible_director/lifecycle_environment_path.rb
+++ app/models/foreman_ansible_director/lifecycle_environment_path.rb
@@ -17,28 +17,6 @@ module ForemanAnsibleDirector
 
     scoped_search on: :name, complete_value: true
 
-    # TODO: Unused. Does this bring advantages anywhere?
-    def swap_environments(env1_id, env2_id)
-      ::ForemanAnsibleDirector::LifecycleEnvironment.transaction do
-        env1 = lifecycle_environments.find(env1_id)
-        env2 = lifecycle_environments.find(env2_id)
-
-        temp_position = env1.position
-        env1.update!(position: env2.position)
-        env2.update!(position: temp_position)
-
-        temp_parent = env1.parent
-        env1.update!(parent: env2.parent)
-        env2.update!(parent: temp_parent)
-
-        env1.children.each { |child| child.update!(parent: env2) }
-        env2.children.each { |child| child.update!(parent: env1) }
-
-        new_root = lifecycle_environments.includes(:parent).find { |env| env.parent.nil? }
-        update!(root_environment: new_root) if new_root
-      end
-    end
-
     def environments_ordered
       lifecycle_environments.order('position')
     end
