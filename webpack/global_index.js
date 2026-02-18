2a2538061b5c4f56fab70ccebb0b51234fbe3240
Thorben Denzer <denzer@atix.de>
2026-02-18 11:03:32 +0100

    feat(ui/i18n): mark auxiliary user-facing strings as translated
    In this commit, every user-facing string in auxiliary components,
    such as AdContext, was reviewed for style and additionally marked
    as translated.
    
    Co-authored-by: Maximilian Kolb <kolb@atix.de>

--- webpack/global_index.js
+++ webpack/global_index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { registerRoutes } from 'foremanReact/routes/RoutingService';
 import { addGlobalFill } from 'foremanReact/components/common/Fill/GlobalFill';
+import { translate as _ } from 'foremanReact/common/I18n';
 import routes from './routes/routes';
 import { HostDetailsTab } from './components/extensions/host_details/HostDetailsTab/HostDetailsTab';
 import { HostDetailsLceCard } from './components/extensions/host_details/HostDetailsLceCard/HostDetailsLceCard';
@@ -9,17 +10,17 @@ import { HostDetailsLceCard } from './components/extensions/host_details/HostDet
 const fills = [
   {
     slot: 'host-details-page-tabs',
-    name: 'Ansible',
+    name: _('Ansible'),
     component: props => <HostDetailsTab {...props} />,
     weight: 500,
     metadata: { title: 'Ansible' },
   },
   {
     slot: 'host-overview-cards',
-    name: 'Ansible Lifecycle environment',
+    name: _('Ansible environment'),
     component: props => <HostDetailsLceCard {...props} />,
     weight: 3700,
-    metadata: { title: 'Ansible' },
+    metadata: { title: _('Ansible') },
   },
 ];
 
