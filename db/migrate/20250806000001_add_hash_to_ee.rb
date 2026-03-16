c6be6981c5971c252bbd78a94fba340caa11154b
Thorben Denzer <denzer@atix.de>
2026-03-16 13:44:01 +0100

    refactor!: squash existing migrations
    The existing migrations serve no purpose anymore and waste time
    by migrating an incorrect schema.
    Since the project is currently in a preview state, this breakage
    is permissible.

--- db/migrate/20250806000001_add_hash_to_ee.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddHashToEe < ActiveRecord::Migration[6.1]
-  def change
-    add_column :ad_execution_environments, :content_hash, :string, null: false, default: ''
-  end
-end
--- db/migrate/20260311000001_ee_status.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class EeStatus < ActiveRecord::Migration[6.1]
-  def change
-    add_column :ad_execution_environments, :build_status, :string, null: true
-    add_column :ad_execution_environments, :build_job, :string, null: true
-
-  end
-end
