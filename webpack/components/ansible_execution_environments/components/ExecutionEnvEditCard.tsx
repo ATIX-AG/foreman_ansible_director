bae3c5009e86a5121d9e64c8a28867e0aa453483
Nadja Heitmann <nadjah@atix.de>
2026-03-17 08:09:43 +0000

    feat(ui/execution_environments): add input validations
    Do not allow to save the Execution Environment to save with empty imput
    fields to prevent errors in the backend.

--- webpack/components/ansible_execution_environments/components/ExecutionEnvEditCard.tsx
+++ webpack/components/ansible_execution_environments/components/ExecutionEnvEditCard.tsx
@@ -1,4 +1,10 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, {
+  Dispatch,
+  SetStateAction,
+  useState,
+  useEffect,
+  FormEvent,
+} from 'react';
 import {
   Card,
   CardTitle,
@@ -21,6 +27,8 @@ import {
 import { ExecutionEnvCardHeaderActions } from './ExecutionEnvCardHeaderActions';
 import { TextInputEditable } from './components/TextInputEditable';
 
+type ValidationState = 'success' | 'warning' | 'error' | 'default';
+
 interface ExecutionEnvEditCardProps {
   editMode: boolean;
   executionEnvironment: AnsibleExecutionEnv | AnsibleExecutionEnvCreate;
@@ -42,12 +50,72 @@ export const ExecutionEnvEditCard = ({
   onNameChange,
   setIsContentUnitModalOpen,
 }: ExecutionEnvEditCardProps): React.ReactElement => {
+  const getDefaultState = (value: string): ValidationState => {
+    if (editMode) return value === '' ? 'default' : 'success';
+    return 'default';
+  };
+
+  const [validateName, setValidateName] = useState<ValidationState>(
+    getDefaultState(executionEnvironment.name)
+  );
+
+  const [validateBaseImageUrl, setValidateBaseImageUrl] = useState<
+    ValidationState
+  >(getDefaultState(executionEnvironment.base_image_url));
+
+  const [validateAnsibleVersion, setValidateAnsibleVersion] = useState<
+    ValidationState
+  >(getDefaultState(executionEnvironment.ansible_version));
+
+  useEffect(() => {
+    setValidateName(getDefaultState(executionEnvironment.name));
+    setValidateBaseImageUrl(
+      getDefaultState(executionEnvironment.base_image_url)
+    );
+    setValidateAnsibleVersion(
+      getDefaultState(executionEnvironment.ansible_version)
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [editMode]);
+
+  const handleNameChange = (
+    _event: FormEvent<HTMLInputElement>,
+    name: string
+  ): void => {
+    onNameChange(name);
+
+    setValidateName(name === '' ? 'error' : 'success');
+  };
+
+  const handleBaseImageChange = (
+    _event: FormEvent<HTMLInputElement>,
+    baseImageUrl: string
+  ): void => {
+    onBaseImageUrlChange(baseImageUrl);
+
+    setValidateBaseImageUrl(baseImageUrl === '' ? 'error' : 'success');
+  };
+
+  const handleAnsibleVersionChange = (
+    _event: FormEvent<HTMLInputElement>,
+    version: string
+  ): void => {
+    onAnsibleVersionChange(version);
+
+    setValidateAnsibleVersion(version === '' ? 'error' : 'success');
+  };
+
   return (
     <Card ouiaId="BasicCard" isLarge isRounded>
       <CardHeader
         actions={{
           actions: (
             <ExecutionEnvCardHeaderActions
+              inputValid={
+                validateName === 'success' &&
+                validateAnsibleVersion === 'success' &&
+                validateBaseImageUrl === 'success'
+              }
               editMode={editMode}
               handleDestroy={handleDestroy}
               handleEdit={handleEdit}
@@ -55,65 +123,71 @@ export const ExecutionEnvEditCard = ({
             />
           ),
         }}
-        type="text"
-        readOnlyVariant={editMode ? undefined : 'plain'}
-        aria-label={_('Environment Name')}
-        id="ansible-dir-ex-env-edit-card"
       />
-    </CardTitle>
-    <CardBody>
-      <Form isHorizontal>
-        <FormGroup label={_('Base image URL')}>
-          <TextInputEditable
-            isEditable={editMode}
-            value={executionEnvironment.base_image_url}
-            setValue={(_event, value) => {
-              onBaseImageUrlChange(value);
-            }}
-          />
-        </FormGroup>
-        <FormGroup
-          label={_('Ansible version')}
-          labelIcon={
-            <Popover
-              headerContent={<div>ansible-core version</div>}
-              bodyContent={
-                <div>
-                  {_(
-                    'The ansible-core version for this Execution Environment.\n' +
-                      'As ansible-core is installed from PyPi, the version must match one of '
-                  )}
-                  <a
-                    href="https://pypi.org/project/ansible-core/#history"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {_('the available releases.')}
-                  </a>
-                </div>
-              }
-            >
-              <button
-                type="button"
-                aria-label={_('More info for unit id field')}
-                onClick={e => e.preventDefault()}
-                aria-describedby="content-unit-identifier-field-01"
-                className={styles.formGroupLabelHelp}
+      <CardTitle>
+        <TextInput
+          validated={validateName}
+          className="pf-v5-c-card__title-text"
+          value={executionEnvironment.name}
+          onChange={handleNameChange}
+          type="text"
+          readOnlyVariant={editMode ? undefined : 'plain'}
+          aria-label={_('Environment Name')}
+          id="ansible-dir-ex-env-edit-card"
+        />
+      </CardTitle>
+      <CardBody>
+        <Form isHorizontal>
+          <FormGroup label={_('Base image URL')} isRequired={editMode}>
+            <TextInputEditable
+              validated={validateBaseImageUrl}
+              isEditable={editMode}
+              value={executionEnvironment.base_image_url}
+              setValue={handleBaseImageChange}
+            />
+          </FormGroup>
+          <FormGroup
+            label={_('Ansible version')}
+            isRequired={editMode}
+            labelIcon={
+              <Popover
+                headerContent={<div>ansible-core version</div>}
+                bodyContent={
+                  <div>
+                    {_(
+                      'The ansible-core version for this Execution Environment.\n' +
+                        'As ansible-core is installed from PyPi, the version must match one of '
+                    )}
+                    <a
+                      href="https://pypi.org/project/ansible-core/#history"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {_('the available releases.')}
+                    </a>
+                  </div>
+                }
               >
-                <HelpIcon />
-              </button>
-            </Popover>
-          }
-        >
-          <TextInputEditable
-            isEditable={editMode}
-            value={executionEnvironment.ansible_version}
-            setValue={(_event, value) => {
-              onAnsibleVersionChange(value);
-            }}
-          />
-        </FormGroup>
-        {/*          <FormGroup label="Baked content"> TODO: evaluate whether this feature should be exposed to users
+                <button
+                  type="button"
+                  aria-label={_('More info for unit id field')}
+                  onClick={e => e.preventDefault()}
+                  aria-describedby="content-unit-identifier-field-01"
+                  className={styles.formGroupLabelHelp}
+                >
+                  <HelpIcon />
+                </button>
+              </Popover>
+            }
+          >
+            <TextInputEditable
+              validated={validateAnsibleVersion}
+              isEditable={editMode}
+              value={executionEnvironment.ansible_version}
+              setValue={handleAnsibleVersionChange}
+            />
+          </FormGroup>
+          {/*          <FormGroup label="Baked content"> TODO: evaluate whether this feature should be exposed to users
             <Bullseye>
               <Button
                 variant="control"
@@ -126,7 +200,8 @@ export const ExecutionEnvEditCard = ({
               </Button>
             </Bullseye>
           </FormGroup> */}
-      </Form>
-    </CardBody>
-  </Card>
-);
+        </Form>
+      </CardBody>
+    </Card>
+  );
+};
--- webpack/components/ansible_execution_environments/components/components/TextInputEditable.tsx
+++ webpack/components/ansible_execution_environments/components/components/TextInputEditable.tsx
@@ -2,17 +2,20 @@ import React, { FormEvent } from 'react';
 import { TextInput } from '@patternfly/react-core';
 
 interface TextInputEditableProps {
+  validated?: 'success' | 'warning' | 'error' | 'default';
   isEditable: boolean;
   value: string | number;
   setValue: (event: FormEvent<HTMLInputElement>, value: string) => void;
 }
 
 export const TextInputEditable: React.FC<TextInputEditableProps> = ({
+  validated = 'default',
   isEditable,
   value,
   setValue,
 }) => (
   <TextInput
+    validated={validated}
     isDisabled={!isEditable}
     value={value}
     type="text"
