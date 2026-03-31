a350bb334b1dedb4bc5f499f743e0278c6b9dd4d
Thorben Denzer <denzer@atix.de>
2026-03-31 17:07:20 +0200

    fix(ui): page title not updating in browser tab for plugin pages
    Before this commit, and since b75f5ef, the tab title did not change when
    navigating between webpages stemming from this plugin.
    
    In b75f5ef, the dependency on react-helmet was removed, without a
    replacement for the functionality being implemented.
    React-helmet used to set document.title, which in turn also fills the
    tab title in the browser.
    
    This is fixed by setting document.title in the Page component, which is used
    by every page of this plugin.

--- webpack/components/common/Page.tsx
+++ webpack/components/common/Page.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import {
   PageSection,
   PageSectionVariants,
@@ -25,46 +25,57 @@ export const Page = ({
   children,
   customToolbarItems,
   searchBar,
-}: PageProps): ReactElement => (
-  <div id="foreman-page">
-    <PageSection
-      variant={PageSectionVariants.light}
-      className="table-title-section"
-    >
-      <TextContent>
-        <Text ouiaId="header-text" component="h1">
-          {header}
-        </Text>
-      </TextContent>
-    </PageSection>
-    <PageSection
-      variant={PageSectionVariants.light}
-      className="table-toolbar-section"
-    >
-      <Toolbar ouiaId="table-toolbar" className="table-toolbar">
-        <ToolbarContent>
-          {searchBar && (
-            <ToolbarGroup
-              className="toolbar-group-search"
-              variant="filter-group"
-            >
-              <ToolbarItem className="toolbar-search">{searchBar}</ToolbarItem>
-            </ToolbarGroup>
-          )}
-          {customToolbarItems && (
-            <ToolbarGroup
-              align={{ default: 'alignRight' }}
-              className="table-toolbar-actions"
-              variant="button-group"
-            >
-              {customToolbarItems && customToolbarItems}
-            </ToolbarGroup>
-          )}
-        </ToolbarContent>
-      </Toolbar>
-    </PageSection>
-    <PageSection variant={PageSectionVariants.light} className="table-section">
-      {children}
-    </PageSection>
-  </div>
-);
+}: PageProps): ReactElement => {
+  useEffect(() => {
+    document.title = header;
+  }, [header]);
+
+  return (
+    <div id="foreman-page">
+      <PageSection
+        variant={PageSectionVariants.light}
+        className="table-title-section"
+      >
+        <TextContent>
+          <Text ouiaId="header-text" component="h1">
+            {header}
+          </Text>
+        </TextContent>
+      </PageSection>
+      <PageSection
+        variant={PageSectionVariants.light}
+        className="table-toolbar-section"
+      >
+        <Toolbar ouiaId="table-toolbar" className="table-toolbar">
+          <ToolbarContent>
+            {searchBar && (
+              <ToolbarGroup
+                className="toolbar-group-search"
+                variant="filter-group"
+              >
+                <ToolbarItem className="toolbar-search">
+                  {searchBar}
+                </ToolbarItem>
+              </ToolbarGroup>
+            )}
+            {customToolbarItems && (
+              <ToolbarGroup
+                align={{ default: 'alignRight' }}
+                className="table-toolbar-actions"
+                variant="button-group"
+              >
+                {customToolbarItems && customToolbarItems}
+              </ToolbarGroup>
+            )}
+          </ToolbarContent>
+        </Toolbar>
+      </PageSection>
+      <PageSection
+        variant={PageSectionVariants.light}
+        className="table-section"
+      >
+        {children}
+      </PageSection>
+    </div>
+  );
+};
