ATIX-AG/foreman_ansible_director

commit f8218c9cb5a6301b7cb702086f326d7221ee8abb

feat(acu)!: use ids for deletion of imported ansible content
Before this commit, the API endpoint responsible for deleting Ansible content
accepted an array of identifiers (i.e. "manala.roles") and an optional array
of versions e.g. ("5.0.0").
While this approach was convenient, it does not hold anymore with the Git source
being available.

Consider the following situation:

1. I import manala.roles@5.2.0 from galaxy.ansible.com
2. I import manala.roles from git, using "5.2.0" as the reference.

Now, two distinct versions of the same collection version are available for use.

If I now wanted to delete either of them, I would not be able to do so with the
original implementation, as the request parameters

identifier="manala.roles"
versions=["5.2.0"]

would be ambiguous, since both collection versions share the same version number.

In this commit, the deletion endpoint has been redesigned to address this problem.
Now, the endpoint accepts a request payload of the following form:

{
  "units": [
    {
      "unit_id": <unit_id>,
      "unit_version_ids": [
        <unit_version_id>
      ]
    }
  ]
}

The "unit_version_ids" key is optional, and when not given, the entire collection,
including every collection version, is removed.
An empty array results in a noop.

Besides this user-facing change, the destroy action has been split into four different
destroy actions, each handling complete/partial destruction of galaxy/git sourced content.
This was done to improve maintainability and separation of concerns.

app/controllers/foreman_ansible_director/api/v2/ansible_content_controller.rb
@@ -4,7 +4,7 @@ module ForemanAnsibleDirector
   module Api
     module V2
       class AnsibleContentController < AnsibleDirectorApiController
-        before_action :find_organization, only: %i[create_units destroy_units]
+        before_action :find_organization, only: %i[create_units]
         before_action :find_optional_organization, only: %i[index]
 
         # TODO: APIDOC
@@ -36,7 +36,7 @@ module ForemanAnsibleDirector
         # TODO: This needs to check and invalidate built EEs
         def destroy_units
           resolved = ::ForemanAnsibleDirector::AnsibleContent::AnsibleContentHelpers.resolve_destroy_payload(
-            params[:units]
+            destroy_params
           )
           @bulk_destroy_task =
             ForemanTasks.sync_task(::ForemanAnsibleDirector::Actions::AnsibleContentUnit::Bulk::Destroy,
@@ -48,6 +48,15 @@ module ForemanAnsibleDirector
         end
 
         private
+
+        def destroy_params
+          params.require(:units).map do |unit|
+            unit.permit(
+              :unit_id,
+              unit_version_ids: []
+            )
+          end
+        end
 
         def validate_requirements_payload
           # TODO: Grammar

app/lib/foreman_ansible_director/actions/ansible_content_unit/bulk/destroy.rb
@@ -12,11 +12,12 @@ module ForemanAnsibleDirector
 
           def plan(args)
             concurrence do
-              args[:resolved_content_units].each do |unit_hash|
+              args[:resolved_content_units].each do |unit_id, instruction|
+                complete = instruction.delete(:complete)
                 plan_action(::ForemanAnsibleDirector::Actions::AnsibleContentUnit::Destroy,
-                  unit: unit_hash[:unit],
-                  content_unit_id: unit_hash[:content_unit_id],
-                  organization_id: args[:organization_id])
+                  unit_id: unit_id,
+                  unit_version_ids: instruction[:versions],
+                  complete: complete)
               end
             end
           end

app/lib/foreman_ansible_director/actions/ansible_content_unit/destroy.rb
@@ -5,101 +5,88 @@ module ForemanAnsibleDirector
     module AnsibleContentUnit
       class Destroy < ::ForemanAnsibleDirector::Actions::Base::AnsibleDirectorAction
         input_format do
-          param :unit
-          param :content_unit_id
-          param :organization_id
+          param :unit_id
+          param :unit_version_ids # {git: [], galaxy: []}
+          param :complete, Boolean
         end
 
         def plan(args)
-          unit = args[:unit]
-          content_unit_id = args[:content_unit_id]
+          unit_id = args[:unit_id]
+          unit_version_ids = args[:unit_version_ids]
 
-          if !unit.versions.empty?
-            plan_partial_destroy(unit, content_unit_id)
+          complete = args[:complete]
+
+          if complete
+            plan_full_destroy(unit_id)
+            plan_self(
+              unit_id: unit_id
+            )
           else
-            plan_full_destroy(content_unit_id)
+            plan_partial_destroy(unit_id, unit_version_ids)
           end
-          plan_self(
-            content_unit_id: content_unit_id,
-            unit_type: unit.unit_type,
-            unit_versions: unit.versions
-          )
         end
 
         def finalize
           acu = ::ForemanAnsibleDirector::ContentUnit.find_by(
-            id: input[:content_unit_id]
+            id: input[:unit_id]
           )
-          if input[:unit_type] == 'collection'
-            if !input[:unit_versions].empty? # partial
-              input[:unit_versions].each do |version|
-                acu&.content_unit_versions&.find_by(version: version)&.destroy
-              end
-            else
-              acu&.destroy
-            end
-          else
-            acu&.destroy
-          end
+          acu&.destroy
         end
 
         private
 
-        def plan_full_destroy(content_unit_id)
-          acu = ::ForemanAnsibleDirector::ContentUnit.find_by(id: content_unit_id)
+        # I am disabling the guard-clause rule here, because I don't think it makes a lot of sense in this case.
+        # galaxy_cuvs and git_cuvs are independent of each other, so early returning is not possible.
+        # rubocop: disable Style/GuardClause
+        def plan_full_destroy(unit_id)
+          galaxy_cuvs = ::ForemanAnsibleDirector::ContentUnit
+                        .find_by(id: unit_id)
+                        .content_unit_versions
+                        .where(source_type: 'galaxy')
 
-          unique_remotes = Set[]
-          unique_repositories = Set[]
-          unique_distributions = Set[]
+          git_cuvs = ::ForemanAnsibleDirector::ContentUnit
+                     .find_by(id: unit_id)
+                     .content_unit_versions
+                     .where(source_type: 'git')
 
-          acu.content_unit_versions.each do |version|
-            unique_remotes.add(version.pulp_remote_href)
-            unique_repositories.add(version.pulp_repository_href)
-            unique_distributions.add(version.pulp_distribution_href)
+          unless galaxy_cuvs.empty?
+            plan_action(::ForemanAnsibleDirector::Actions::AnsibleContentUnit::DestroyProviders::Galaxy::DestroyFull,
+              unit_id: unit_id,
+              unit_version_ids: galaxy_cuvs)
           end
 
-          unique_remotes = unique_remotes.to_a
-          unique_repositories = unique_repositories.to_a
-          unique_distributions = unique_distributions.to_a
-
-          # unique_* all have the same length, neither of these objects can be created without the other ones
-          concurrence do
-            unique_remotes.length.times do |n|
-              plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Repository::Destroy,
-                repository_href: unique_repositories[n])
-              plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Distribution::Destroy,
-                distribution_href: unique_distributions[n])
-
-              if acu.collection?
-                plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Remote::Collection::Destroy,
-                  collection_remote_href: unique_remotes[n])
-              else
-                plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Remote::Role::Destroy,
-                  role_remote_href: unique_remotes[n])
-              end
+          unless git_cuvs.empty?
+            git_cuvs.each do |git_cuv|
+              plan_action(::ForemanAnsibleDirector::Actions::AnsibleContentUnit::DestroyProviders::Git::DestroyFull,
+                unit_id: unit_id,
+                unit_version_id: git_cuv)
             end
           end
         end
+        # rubocop: enable Style/GuardClause
 
-        def plan_partial_destroy(unit, content_unit_id)
-          acu = ::ForemanAnsibleDirector::AnsibleCollection.find_by(id: content_unit_id) # Only collections
+        # Same as above
+        # rubocop: disable Style/GuardClause
+        def plan_partial_destroy(unit_id, unit_version_ids)
+          galaxy_cuvs = unit_version_ids[:galaxy]
+          git_cuvs = unit_version_ids[:git]
 
-          repository_href = acu.content_unit_versions.first.pulp_repository_href
-          remote_href = acu.content_unit_versions.first.pulp_remote_href
+          unless galaxy_cuvs.empty?
+            plan_action(::ForemanAnsibleDirector::Actions::AnsibleContentUnit::DestroyProviders::Galaxy::DestroyPartial,
+              unit_id: unit_id,
+              unit_version_ids: galaxy_cuvs)
+          end
 
-          sequence do
-            _remote_update_action = plan_action(
-              ::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Remote::Collection::Update,
-              collection_remote_href: remote_href,
-              requirements: acu.requirements_file(unit, subtractive: true)
-            )
-            _snyc_action = plan_action(
-              ::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Repository::Sync,
-              repository_href: repository_href,
-              remote_href: remote_href
-            )
+          unless git_cuvs.empty?
+            git_cuvs.each do |git_cuv|
+              plan_action(::ForemanAnsibleDirector::Actions::AnsibleContentUnit::DestroyProviders::Git::DestroyPartial,
+                unit_id: unit_id,
+                unit_version_id: git_cuv)
+            end
+
           end
         end
+        # rubocop: enable Style/GuardClause
       end
     end
   end

app/lib/foreman_ansible_director/actions/ansible_content_unit/destroy_providers/galaxy/destroy_full.rb
@@ -1,0 +1,43 @@
+# frozen_string_literal: true
+
+module ForemanAnsibleDirector
+  module Actions
+    module AnsibleContentUnit
+      module DestroyProviders
+        module Galaxy
+          class DestroyFull < ::ForemanAnsibleDirector::Actions::Base::AnsibleDirectorAction
+            input_format do
+              param :unit_id
+              param :unit_version_ids
+            end
+
+            def plan(args)
+              unit_id = args[:unit_id]
+
+              # Since Pulp objects are the same for content imported from Galaxy, any CUV will do
+              cuv = ::ForemanAnsibleDirector::ContentUnitVersion.where(
+                versionable_id: unit_id,
+                source_type: 'galaxy'
+              ).includes(:versionable).first
+
+              concurrence do
+                plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Repository::Destroy,
+                  repository_href: cuv.pulp_repository_href)
+                plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Distribution::Destroy,
+                  distribution_href: cuv.pulp_distribution_href)
+
+                if cuv.versionable.collection?
+                  plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Remote::Collection::Destroy,
+                    collection_remote_href: cuv.pulp_remote_href)
+                else
+                  plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Remote::Role::Destroy,
+                    role_remote_href: cuv.pulp_remote_href)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end

app/lib/foreman_ansible_director/actions/ansible_content_unit/destroy_providers/galaxy/destroy_partial.rb
@@ -1,0 +1,53 @@
+# frozen_string_literal: true
+
+module ForemanAnsibleDirector
+  module Actions
+    module AnsibleContentUnit
+      module DestroyProviders
+        module Galaxy
+          class DestroyPartial < ::ForemanAnsibleDirector::Actions::Base::AnsibleDirectorAction
+            input_format do
+              param :unit_id
+              param :unit_version_ids # []
+            end
+
+            def plan(args)
+              unit_version_ids = args[:unit_version_ids]
+
+              cuvs = ::ForemanAnsibleDirector::ContentUnitVersion.where(id: unit_version_ids)
+
+              versions_to_remove = cuvs.pluck(:version)
+              partial_scu = Struct.new(:versions).new(versions_to_remove)
+
+              # Shared between all versions_to_remove
+              cuv = cuvs[0]
+              acu = cuv.versionable
+
+              new_requirements = acu.requirements_file(partial_scu, subtractive: true)
+              remote_href = cuv.pulp_remote_href
+              repository_href = cuv.pulp_repository_href
+
+              _remote_update_action = plan_action(
+                ::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Remote::Collection::Update,
+                collection_remote_href: remote_href,
+                requirements: new_requirements
+              )
+              _snyc_action = plan_action(
+                ::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Repository::Sync,
+                repository_href: repository_href,
+                remote_href: remote_href
+              )
+
+              plan_self(unit_version_ids: unit_version_ids)
+            end
+
+            def finalize
+              unit_version_ids = input[:unit_version_ids]
+              ::ForemanAnsibleDirector::ContentUnitVersion.where(id: unit_version_ids).destroy_all
+            end
+          end
+        end
+      end
+    end
+  end
+end

app/lib/foreman_ansible_director/actions/ansible_content_unit/destroy_providers/git/destroy_full.rb
@@ -1,0 +1,34 @@
+# frozen_string_literal: true
+
+module ForemanAnsibleDirector
+  module Actions
+    module AnsibleContentUnit
+      module DestroyProviders
+        module Git
+          class DestroyFull < ::ForemanAnsibleDirector::Actions::Base::AnsibleDirectorAction
+            input_format do
+              param :unit_id
+              param :unit_version_id
+            end
+
+            def plan(args)
+              unit_version_id = args[:unit_version_id]
+
+              cuv = ::ForemanAnsibleDirector::ContentUnitVersion.find_by(id: unit_version_id)
+
+              concurrence do
+                plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Repository::Destroy,
+                  repository_href: cuv.pulp_repository_href)
+                plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Distribution::Destroy,
+                  distribution_href: cuv.pulp_distribution_href)
+
+                plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Remote::Git::Destroy,
+                  git_remote_href: cuv.pulp_remote_href)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end

app/lib/foreman_ansible_director/actions/ansible_content_unit/destroy_providers/git/destroy_partial.rb
@@ -1,0 +1,41 @@
+# frozen_string_literal: true
+
+module ForemanAnsibleDirector
+  module Actions
+    module AnsibleContentUnit
+      module DestroyProviders
+        module Git
+          class DestroyPartial < ::ForemanAnsibleDirector::Actions::Base::AnsibleDirectorAction
+            input_format do
+              param :unit_id
+              param :unit_version_id
+            end
+
+            def plan(args)
+              unit_version_id = args[:unit_version_id]
+
+              cuv = ::ForemanAnsibleDirector::ContentUnitVersion.find_by(id: unit_version_id)
+
+              concurrence do
+                plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Repository::Destroy,
+                  repository_href: cuv.pulp_repository_href)
+                plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Distribution::Destroy,
+                  distribution_href: cuv.pulp_distribution_href)
+
+                plan_action(::ForemanAnsibleDirector::Actions::Pulp3::Ansible::Remote::Git::Destroy,
+                  git_remote_href: cuv.pulp_remote_href)
+              end
+
+              plan_self(unit_version_id: unit_version_id)
+            end
+
+            def finalize
+              unit_version_id = input[:unit_version_id]
+              ::ForemanAnsibleDirector::ContentUnitVersion.find_by(id: unit_version_id)&.destroy
+            end
+          end
+        end
+      end
+    end
+  end
+end

app/lib/foreman_ansible_director/ansible_content/ansible_content_helpers.rb
@@ -22,44 +22,51 @@ module ForemanAnsibleDirector
         end
 
         def resolve_destroy_payload(payload)
-          units = {}
+          unit_ids = payload.map { |u| u[:unit_id] }
+
+          units = ::ForemanAnsibleDirector::ContentUnit
+                  .where(id: unit_ids)
+                  .includes(:content_unit_versions)
+                  .index_by(&:id)
+
+          unit_versions = Hash.new { |h, k| h[k] = { versions: { galaxy: Set.new, git: Set.new }, complete: false } }
 
           payload.each do |unit|
-            name = unit[:unit_name]
+            unit_id = unit[:unit_id]
+            unit_record = units[unit_id]
+            raise(ActiveRecord::RecordNotFound, "Content unit with id #{unit_id} not found") unless unit_record
 
-            unless name.match?(/^(.*)\.(.*)$/)
-              raise "Invalid unit name format: #{name}" # TODO: Proper error code
-            end
+            all_version_ids = unit_record.content_unit_versions.pluck(:id).to_set
 
-            unit_namespace, unit_name = name.match(/^(.*)\.(.*)$/).captures
-            existing_unit = ::ForemanAnsibleDirector::ContentUnit.find_by(namespace: unit_namespace, name: unit_name)
+            if (requested_versions = unit[:unit_version_ids]).nil?
+              unit_versions[unit_id][:complete] = true # Key not given -> Full deletion
+            else
+              requested_versions.each do |version_id|
+                cuv = ContentUnitVersion.find_by(id: version_id)
+                raise(ActiveRecord::RecordNotFound, "Version with id #{version_id} not found") unless cuv
 
-            raise "Unit not found: #{name}" unless existing_unit # TODO: Proper error code
-
-            unit_type = existing_unit.collection? ? :collection : :role
-
-            versions = []
-
-            if unit_type == :collection
-              versions = unit[:unit_versions]&.select do |version|
-                existing_unit.content_unit_versions.find_by(version: version)
-              end
-
-              if versions && versions.length == existing_unit.content_unit_versions.count
-                versions = [] # In this case, we are deleting the complete unit
+                source_type = cuv.source_type.to_sym
+                unit_versions[unit_id][:versions][source_type].add(version_id)
               end
             end
 
-            simple_unit = SimpleAnsibleContentUnit.new(
-              unit_type: unit_type,
-              unit_name: name,
-              unit_versions: versions
-            )
+            supplied_galaxy_versions = unit_versions[unit_id][:versions][:galaxy]
+            supplied_git_versions = unit_versions[unit_id][:versions][:git]
 
-            units[name] = { unit: simple_unit, content_unit_id: existing_unit.id }
+            if (supplied_galaxy_versions + supplied_git_versions).to_set.superset?(all_version_ids)
+              unit_versions[unit_id][:complete] = true
+            end
           end
 
-          units.values
+          unit_versions.transform_values do |types|
+            {
+              versions: {
+                galaxy: types[:versions][:galaxy].to_a,
+                git: types[:versions][:git].to_a,
+              },
+              complete: types[:complete],
+            }
+          end
         end
 
         def resolve_import_payload(payload)

commit a5cb8404a8bd6c47ab6f17a8f18ee1a9fbe85cc5

fix(acu): missing organization when deleting content
Before this commit, deletion of Ansible content crashed with
an error, mentioning a missing organization.

This was caused in commit f8218c9cb5a6301b7cb702086f326d7221ee8abb,
where deletion of Ansible content was changed to use IDs instead
of names.
This meant that an organization_id no longer had to be passed with
the DELETE API request. As such, the query for the organization
object was also removed.
Unfortunately, the unset @organization object was still referenced,
which led to this error.

This is fixed by removing the reference to the @organization object.

app/controllers/foreman_ansible_director/api/v2/ansible_content_controller.rb
@@ -40,7 +40,7 @@ module ForemanAnsibleDirector
           )
           @bulk_destroy_task =
             ForemanTasks.sync_task(::ForemanAnsibleDirector::Actions::AnsibleContentUnit::Bulk::Destroy,
-              resolved_content_units: resolved, organization_id: @organization.id)
+              resolved_content_units: resolved)
         end
 
         def model_of_controller