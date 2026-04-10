1769d946d0f1df0a2fa4efca4b5910e99d10dbf6
Nadja Heitmann <nadjah@atix.de>
2026-04-10 15:18:12 +0200

    fix(reports): do not overwrite config report host

--- app/services/foreman_ansible_director/ansible_report_importer.rb
+++ app/services/foreman_ansible_director/ansible_report_importer.rb
@@ -4,9 +4,6 @@ module ForemanAnsibleDirector
   module AnsibleReportImporter
     extend ActiveSupport::Concern
     included do
-      def host
-        Rails.logger.debug raw
-      end
     end
   end
 end
