f9af611beed5e2417c9e90e4362085497076d035
Thorben Denzer <denzer@atix.de>
2026-04-15 15:41:59 +0200

    fix(pulp): pulp ansible api client connection failure
    Before this commit, if the foreman_azure plugin was installed
    alongside this plugin, importing of Ansible content would
    always fail with the error message "Connection failed".
    
    This was caused by the request library used by
    pulp_ansible_client (Faraday) using the "excon" adapter
    for performing requests.
    Excon differs in it's API from the Net::HTTP adapter in
    the following way:
    
    When the Net:HTTP adapter is used, the ssl_client_cert
    and ssl_client_key parameters of the PulpcoreClient:: and
    PulpAnsibleClient::Configuration objects require passing of
    an OpenSSL::X509::Certificate object.
    When the Excon adapter is used, these parameters must
    be paths to the certificates.
    
    This was not handled, causing the following error message:
    "no implicit conversion of OpenSSL::X509::Certificate into
    String (TypeError) (Faraday::ConnectionFailed)".
    
    This is fixed by passing the correct values (certificate object/
    certificate path), depending on the adapter used by Faraday.
    
    Refs:
    
    - https://github.com/Katello/katello/commit/d2b75839705a72a7970f732c7d7159a48125f3fe
    - https://projects.theforeman.org/issues/30746

--- app/services/foreman_ansible_director/cert/certs.rb
+++ app/services/foreman_ansible_director/cert/certs.rb
@@ -7,12 +7,20 @@ module ForemanAnsibleDirector
         Setting[:ssl_ca_file]
       end
 
+      def self.ssl_client_cert_file
+        Setting[:ssl_certificate]
+      end
+
+      def self.ssl_client_key_file
+        Setting[:ssl_priv_key]
+      end
+
       def self.ssl_client_cert
-        OpenSSL::X509::Certificate.new(File.read(Setting[:ssl_certificate]))
+        OpenSSL::X509::Certificate.new(File.read(ssl_client_cert_file))
       end
 
       def self.ssl_client_key
-        OpenSSL::PKey::RSA.new(File.read(Setting[:ssl_priv_key]))
+        OpenSSL::PKey::RSA.new(File.read(ssl_client_key_file))
       end
     end
   end
