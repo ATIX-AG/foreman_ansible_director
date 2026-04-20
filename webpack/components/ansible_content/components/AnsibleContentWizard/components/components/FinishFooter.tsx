c64a766e8788b63c97decc1e9227f0c8efa74201
Nadja Heitmann <nadjah@atix.de>
2026-04-20 13:01:04 +0200

    fix(ui/ansible_content): fix typo in toast message

--- webpack/components/ansible_content/components/AnsibleContentWizard/components/components/FinishFooter.tsx
+++ webpack/components/ansible_content/components/AnsibleContentWizard/components/components/FinishFooter.tsx
@@ -51,7 +51,7 @@ const FinishFooter: React.FC<FinishFooterProps> = ({
             type: 'success',
             key: `IMPORT_CU_${contentUnits.length}_SUCC`,
             message:
-              'Sucessfully imported Ansible content units from YAML file"!',
+              'Successfully imported Ansible content units from YAML file"!',
             sticky: false,
           })
         );
@@ -89,7 +89,7 @@ const FinishFooter: React.FC<FinishFooterProps> = ({
           addToast({
             type: 'success',
             key: `IMPORT_CU_${contentUnits.length}_SUCC`,
-            message: `Sucessfully imported ${
+            message: `Successfully imported ${
               contentUnits.length
             } Ansible content ${
               contentUnits.length === 1 ? 'unit' : 'units'
