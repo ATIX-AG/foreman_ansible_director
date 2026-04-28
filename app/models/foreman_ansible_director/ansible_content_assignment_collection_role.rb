a7a0064d17011be715be7af303492e9469fdaeb3
Thorben Denzer <denzer@atix.de>
2026-04-28 15:11:08 +0200

    refactor(assignments): remove unused assignment model

--- app/models/foreman_ansible_director/ansible_content_assignment_collection_role.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module ForemanAnsibleDirector
-  class AnsibleContentAssignmentCollectionRole < ::ForemanAnsibleDirector::AnsibleDirectorModel
-    belongs_to :ansible_content_assignment
-    belongs_to :ansible_collection_role
-  end
-  # TODO: Delete class - assignments are handled via generic assignment model
-end
