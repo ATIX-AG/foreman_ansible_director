4bfea6ba3714f99d6546ffa9e03953713013daea
Nadja Heitmann <nadjah@atix.de>
2026-04-30 08:43:48 +0000

    fix(ui/host_details): align details card with Foreman style
    * use Foreman edit/submit icons
    * total width of selector aligned to card
    * remove duplicate LCE selector from header

--- webpack/components/extensions/host_details/HostDetailsLceCard/components/HostDetailsLceCardHeaderActions.tsx
+++ webpack/components/extensions/host_details/HostDetailsLceCard/components/HostDetailsLceCardHeaderActions.tsx
@@ -1,12 +1,8 @@
-import { Icon, ToggleGroup, ToggleGroupItem } from '@patternfly/react-core';
-import SaveIcon from '@patternfly/react-icons/dist/esm/icons/save-icon';
-import EditIcon from '@patternfly/react-icons/dist/esm/icons/edit-icon';
-import CatalogIcon from '@patternfly/react-icons/dist/esm/icons/catalog-icon';
-import BundleIcon from '@patternfly/react-icons/dist/esm/icons/bundle-icon';
+import React from 'react';
+import { Icon, Tooltip } from '@patternfly/react-core';
+import { CheckIcon, PencilAltIcon } from '@patternfly/react-icons';
 
 import { translate as _ } from 'foremanReact/common/I18n';
-
-import React from 'react';
 import { PermittedButton } from '../../../../common/PermittedButton';
 import { AdPermissions } from '../../../../../constants/foremanAnsibleDirectorPermissions';
 
@@ -14,56 +10,24 @@ interface HostDetailsLceCardHeaderActionsProps {
   isEditMode: boolean;
   handleEdit: () => void;
   isUsingLibrary: boolean;
-  handleContentSourceSet: () => void;
 }
 
 export const HostDetailsLceCardHeaderActions: React.FC<HostDetailsLceCardHeaderActionsProps> = ({
   isEditMode,
   handleEdit,
   isUsingLibrary,
-  handleContentSourceSet,
-}) => (
-  <>
-    <ToggleGroup
-      areAllGroupsDisabled={false}
-      aria-label="Default with multiple selectable"
-    >
-      <ToggleGroupItem
-        icon={isUsingLibrary ? <CatalogIcon /> : <BundleIcon />}
-        text={isUsingLibrary ? _('Library') : _('Lifecycle environment')}
-        key={0}
-        buttonId="toggle-group-multiple-1"
-        isSelected={isUsingLibrary}
-        onChange={() => handleContentSourceSet()}
-      />
-    </ToggleGroup>
-    {!isUsingLibrary && (
-      <PermittedButton
-        requiredPermissions={[AdPermissions.assignments.create]}
-        hasPopover
-        popoverProps={{
-          triggerAction: 'hover',
-          'aria-label': 'edit host lce popover',
-          headerComponent: 'h1',
-          headerContent: _('Assign lifecycle environment'),
-          bodyContent: (
-            <div>{_('Set the lifecycle environment of this host.')}</div>
-          ),
-        }}
-        variant="plain"
-        aria-label="Action"
-        onClick={handleEdit}
-      >
-        {isEditMode ? (
-          <Icon size="lg">
-            <SaveIcon />
-          </Icon>
-        ) : (
-          <Icon size="lg">
-            <EditIcon />
-          </Icon>
-        )}
-      </PermittedButton>
-    )}
-  </>
-);
+}) =>
+  !isUsingLibrary ? (
+    <>
+      <Tooltip content={isEditMode ? _('Submit') : _('Edit')}>
+        <PermittedButton
+          requiredPermissions={[AdPermissions.assignments.create]}
+          variant="plain"
+          aria-label={isEditMode ? _('Submit') : _('Edit')}
+          onClick={() => handleEdit(false)}
+        >
+          <Icon>{isEditMode ? <CheckIcon /> : <PencilAltIcon />}</Icon>
+        </PermittedButton>
+      </Tooltip>
+    </>
+  ) : null;
