851f76ab1db541f9a6568c85cb9547eb1fa5a58f
Thorben Denzer <denzer@atix.de>
2026-05-29 09:58:00 +0200

    fix(variables): convert HashWithIndifferentAccess to Hash before saving
    Before this commit, Hash values of Ansible variables were
    de- and reserialized to HashWithIndifferentAccess
    between the ExtractVariables and IndexStatic actions
    by Dynflow.
    
    This resulted in ActiveRecord storing this value as a
    HashWithIndifferentAccess in the database. This caused
    issues during resolution of Ansible variable overrides.
    
    In this commit, HashWithIndifferentAccess values are
    converted to Hash before saving.

--- app/models/foreman_ansible_director/ansible_variable.rb
+++ app/models/foreman_ansible_director/ansible_variable.rb
@@ -5,6 +5,16 @@ module ForemanAnsibleDirector
     belongs_to :ansible_role, optional: true
     belongs_to :ansible_collection_role, optional: true
 
+    def default_value=(value)
+      super(
+        if value.is_a?(ActiveSupport::HashWithIndifferentAccess)
+          value.to_h
+        else
+          value
+        end
+      )
+    end
+
     def overridable?
       override || lookup_values.count.positive?
     end
