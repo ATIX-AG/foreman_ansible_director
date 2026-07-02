3e77a587a889e61db6a7e2d31a0ac00c2ad83fae
Thorben Denzer <denzer@atix.de>
2026-07-02 12:09:08 +0200

    refactor(ui): centralize toast definitions
    Before this commit, every toast created by the frontend of this plugin
    used a per-case implementation, which made certain parts of the code
    bloated and cumbersome to read.
    
    This refactor adresses this issue by centrally defining toast objects
    used to show the results of CRUD operations, which are by far the most
    common types of toasts.
    These objects are adaptive to the resource type and operation performed
    against it. By being easily reusable, most bespoke toasts can be
    replaced with one of the generated toasts introduced in this commit.

---
+++ webpack/resources/utils.ts
@@ -0,0 +1,5 @@
+export type resourceIdentifier = 'execution_environment';
+
+export const resourceIdDisplayNameMap: Record<resourceIdentifier, string> = {
+  execution_environment: 'Execution Environment',
+};
