2ec6054e47dccd6c2dcdc8614051afb4dfa8d46b
Thorben Denzer <denzer@atix.de>
2026-07-02 12:04:25 +0200

    feat(issues): propagate issues and crud events through api responses
    Before this commit, issues (Errors, Warnings) were only being returned
    in some cases. Further, the response messages indicating creation,
    destruction and updating of objects were inconsistent.
    
    In this commit, the shapes of the "created", "updated" and "deleted"
    values are expanded to further include all relevant data of the object
    they describe.
    
    Secondly, the "errors" and "warning" values are further refined to also
    include all relevant data.

--- app/lib/foreman_ansible_director/issues/warnings/base_warning.rb
+++ app/lib/foreman_ansible_director/issues/warnings/base_warning.rb
@@ -3,7 +3,15 @@
 module ForemanAnsibleDirector
   module Issues
     module Warnings
-      class BaseWarning < BaseIssue; end
+      class BaseWarning < BaseIssue
+        def render_for_response
+          {
+            type: 'warning',
+            title: title,
+            message: message,
+          }
+        end
+      end
     end
   end
 end
