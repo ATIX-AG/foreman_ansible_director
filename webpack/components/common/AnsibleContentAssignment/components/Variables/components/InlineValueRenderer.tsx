8aaa33b64f08877409788b7ce872a18478ec353a
Thorben Denzer <denzer@atix.de>
2026-09-11 15:15:46 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! feat(variables)!: dynamic variable system with bindings and resolution
    Validate YAML and given data type in InlineRenderer

--- webpack/components/common/AnsibleContentAssignment/components/Variables/components/InlineValueRenderer.tsx
+++ webpack/components/common/AnsibleContentAssignment/components/Variables/components/InlineValueRenderer.tsx
@@ -1,7 +1,10 @@
 import React, { ReactElement } from 'react';
 import { Button, CodeBlock, CodeBlockCode, Popover, TextInput, Truncate } from '@patternfly/react-core';
-import { load } from 'js-yaml';
-import { AnsibleVariable, AnsibleVariableBinding } from '../../../../../../types/AnsibleVariableTypes';
+import {
+  AnsibleVariable,
+  AnsibleVariableBinding,
+} from '../../../../../../types/AnsibleVariableTypes';
+import { getYamlValidity, YamlValidity } from '../utils';
 
 interface InlineValueRendererProps {
   item: AnsibleVariable | AnsibleVariableBinding;
@@ -11,45 +14,51 @@ export const InlineValueRenderer = ({
   item,
 }: InlineValueRendererProps): ReactElement => {
 
+  const defaultRenderer = (
+    <Popover
+      hasAutoWidth
+      triggerAction="hover"
+      aria-label="Hoverable popover"
+      headerContent={<div>Effective value</div>}
+      bodyContent={
+        <div style={{ width: '20vw' }}>
+          <CodeBlock>
+            <CodeBlockCode id="code-content">
+              {item.raw_value}
+            </CodeBlockCode>
+          </CodeBlock>
+        </div>
+      }
+    >
+      <Button variant={'secondary'}>Hover to preview value</Button>
+    </Popover>
+  );
+
   const valuePreview = (item: AnsibleVariable | AnsibleVariableBinding): ReactElement => {
-    let loadedValue;
+    let loaded: YamlValidity = getYamlValidity(item.raw_value, item.data_type);
+
+    // catches bad YAML should it somehow get into the system and type mismatches
+    if (loaded.state !== 'valid') {
+      return defaultRenderer;
+    }
+
     switch (item.data_type) {
       case 'boolean':
       case 'integer':
       case 'float':
-        loadedValue = load(item.raw_value) as string;
         return (
-          <TextInput value={loadedValue} type="text" aria-label="disabled text input example" isDisabled />
+          <TextInput value={loaded.parsed as string} type="text" aria-label="disabled text input example" isDisabled />
         );
       case 'string':
-        loadedValue = load(item.raw_value) as string;
         return (
           <Truncate
-            content={loadedValue}
+            content={loaded.parsed as string}
             trailingNumChars={10}
             position={'middle'}
           />
         );
       default:
-        return (
-          <Popover
-            hasAutoWidth
-            triggerAction="hover"
-            aria-label="Hoverable popover"
-            headerContent={<div>Effective value</div>}
-            bodyContent={
-              <div style={{ width: '20vw' }}>
-                <CodeBlock>
-                  <CodeBlockCode id="code-content">
-                    {item.raw_value}
-                  </CodeBlockCode>
-                </CodeBlock>
-              </div>
-            }
-          >
-            <Button variant={'secondary'}>Hover to preview value</Button>
-          </Popover>
-        );
+        return defaultRenderer;
     }
   };
 
